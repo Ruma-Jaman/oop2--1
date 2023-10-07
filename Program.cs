bb54c79bb2d1f1c2afa528cd6997c6e390aad591
Ruma <77885035+Ruma-Jaman@users.noreply.github.com>
2023-10-07 20:54:57 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class student
+    {
+        private int id;
+        private string name;
+        private double cgpa;
+        private string bloodgroup;
+        public int GetId()
+        {
+            return this.id;
+
+        }
+        public student()
+    }
+    public void SetId()
+
+    {
+        static void Main(string[] args)
+        {
+            student s1 = new student();
+            s1.id = 100;
+            s1.name = "bruce";
+            s1.cgpa = "3.24";
+            s1.bloodgroup = "B+";
+            s1.PrintStudentInfo();
+
+        }
+        private void Inputmethod()
+        {
+            this.SetId = convert.ToInt32(Console.ReadLine())
+        }
+        {
+            public student (int id, string name, double cgpa, string bloodgroup );
+            {
+                this.id = id;
+                this.name = name;
+                this.cgpa = cgpa;
+                this.bloodgroup = bloodgroup;
+
+            }
+            {
+                Console.WriteLine("Hello World!");
+        }
+    }
+}
